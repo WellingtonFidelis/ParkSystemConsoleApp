2b849a4ffc24de19260d5ea419c56b83174bf696
WellingtonFidelis <wellingtonf20@gmail.com>
2024-08-18 20:01:46 -0300

    feat: adding methods to the Parking class and done the program menu.

---
+++ Models/Parking.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkSystemConsoleApp.Models.Parking
+{
+    internal class Parking
+    {
+        private decimal initialPrice = 0;
+        private decimal pricePerHour = 0;
+        private List<string> cars = new List<string>();
+
+        public Parking(decimal initialPrice, decimal pricePerHour)
+        {
+            this.initialPrice = initialPrice;
+            this.pricePerHour = pricePerHour;
+        }
+
+        public void AddCar()
+        {
+            Console.WriteLine("Informe o código da placa do veículo para estacionar:");
+            string? licensePlate = Console.ReadLine();
+
+            if (licensePlate != null)
+            {
+                cars.Add(licensePlate.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido");
+            }
+        }
+
+        public void DeleteCar()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+
+            // Pedir para o usuário digitar a placa e armazenar na variável placa
+            // *IMPLEMENTE AQUI*
+            string licensePlate = "";
+
+            // Verifica se o veículo existe
+            if (cars.Any(x => x.ToUpper() == licensePlate.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+                // *IMPLEMENTE AQUI*
+                int hours = 0;
+                decimal totalValue = 0;
+
+                // TODO: Remover a placa digitada da lista de veículos
+                // *IMPLEMENTE AQUI*
+
+                Console.WriteLine($"O veículo {licensePlate} foi removido e o preço total foi de: R$ {totalValue}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+
+        }
+
+        public void ListCar()
+        {
+            if (cars.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são: ");
+
+                foreach (string car in cars)
+                {
+                   Console.WriteLine(car); 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ParkSystemConsoleApp.Models.Parking; 
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal initialPrice = 0;
+decimal pricePerHour = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n");
+Console.WriteLine("Digite o preço inicial:");
+initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+
+Console.WriteLine("Agora digite o preço por hora:");
+pricePerHour = Convert.ToDecimal(Console.ReadLine());
+
+
+Parking pk = new Parking(initialPrice, pricePerHour);
+
+string option = string.Empty;
+bool showMenu = true;
+
+while (showMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            pk.AddCar();
+            break;
+
+        case "2":
+            pk.DeleteCar();
+            break;
+
+        case "3":
+            pk.ListCar();
+            break;
+
+        case "4":
+            showMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+
+Console.WriteLine("O programa encerrou.");
